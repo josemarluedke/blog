0a55091fb8821a035f8e710883a11912692eb3ad
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-11-01 14:33:44 -0200

    added blueprint for user

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -7,3 +7,10 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+User.blueprint do
+  name {'Josemar Davi Luedke'}
+  email {'josemarluedke@gmail.com'}
+  password {'josemar'}
+  password_confirmation {'josemar'}
+end
