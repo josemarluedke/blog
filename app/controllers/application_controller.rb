12b15b37a931cdae922c64e32fa9d3f0d8573354
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-11-05 17:06:10 -0200

    added avatar_url helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :avatar_url
+
+  def avatar_url
+    "http://gravatar.com/avatar/#{Digest::MD5.new.update('josemarluedke@gmail.com')}.jpg?s=220"
+  end
 
   protected
   def render_404
