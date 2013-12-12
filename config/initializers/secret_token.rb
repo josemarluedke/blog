303e4b7de547c9ea899db17a6169b13ba2a06c14
Patrick Müller <patrick.e.muller@gmail.com>
2013-12-12 18:43:33 -0200

    Generated new secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '73a9c2431d408f6961f6afcae04099b8152a3911bba52425bd17bba3d9b4813d057fe7cab5643091f299b75700475f6af6a0dfa48da06360bf6e601ff791b2ed'
+Blog::Application.config.secret_key_base = '3f000e057cff9af4ecba1abb318e4bc510f6da0b17fb22f6389b0503053708812c302fd4e20fb8f85b20dbdaa69858fd7ad4bdef211cd18a6a78bc95b70dba1b'
